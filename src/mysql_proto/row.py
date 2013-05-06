f33beae844bac7dcbc04f3cf3ef73b2ec446be34
Rob Smith <kormoc@gmail.com>
2013-05-06 15:55:07 -0700

    Basic Row

---
+++ src/mysql_proto/row.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from packet import Packet
+from proto import Proto
+from flags import Flags
+
+class Row(Packet):
+    rowType = Flags.ROW_TYPE_TEXT
+    colType = Flags.MYSQL_TYPE_VAR_STRING
+    data = list()
+    
+    def __init__(self, *args, **kwargs):
+        for x in args:
+            self.data.append(x)
+        for x in kwargs:
+            self.data.append(x)
+    
+    def getPayload(self):
+        payload = bytearray()
+        
+        for col in self.data:
+            if self.rowType == Flags.ROW_TYPE_TEXT:
+                if isinstance(col, basestring):
+                    payload.extend(Proto.build_lenenc_str(col))
+                elif isinstance(col, (int, long)):
+                    payload.extend(Proto.build_lenenc_int(col))
+                elif isinstance(col, (float, complex)):
+                    payload.extend(Proto.build_lenenc_str(str(col)))
+                else:
+                    raise NotImplementedError()
+            elif self.rowType == Flags.ROW_TYPE_BINARY:
+                raise NotImplementedError()
+            else:
+                raise NotImplementedError()
+        
+        return payload
+    
+    @staticmethod
+    def loadFromPacket(packet):
+        obj = Row()
+        proto = Proto(packet, 3)
+        
+        obj.sequenceId = proto.get_fixed_int(1)
+        
+        # TODO: Extract row data here
+        
+        return obj
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
