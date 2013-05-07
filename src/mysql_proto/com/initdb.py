c314cdee33b75907d8dfec82c00937ec79f5f48d
Rob Smith <kormoc@gmail.com>
2013-05-07 16:57:41 -0700

    Com_Initdb

---
+++ src/mysql_proto/com/initdb.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from packet import Packet
+from proto import Proto
+from flags import Flags
+
+class Initdb(Packet):
+    schema = ""
+    
+    def getPayload(self):
+        payload = bytearray()
+        
+        payload.extend(Proto.build_byte(Flags.COM_INIT_DB))
+        payload.extend(Proto.build_eop_str(self.schema))
+        
+        return payload
+    
+    @staticmethod
+    def loadFromPacket(packet):
+        obj = Initdb()
+        proto = Proto(packet, 3)
+        
+        obj.sequenceId = proto.get_fixed_int(1)
+        proto.get_filler(1)
+        obj.schema = proto.get_eop_str()
+        
+        return obj
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
