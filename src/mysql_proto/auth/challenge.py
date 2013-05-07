2380df4a9026dff58cf2087a994049e55893a448
Rob Smith <kormoc@gmail.com>
2013-05-07 14:37:11 -0700

    Implement Auth

--- src/mysql_proto/auth/challenge.py
+++ src/mysql_proto/auth/challenge.py
@@ -1,6 +1,85 @@
+#!/usr/bin/env python
 # coding=utf-8
 
-import mysql_proto.flags.Flags
+from packet import Packet
+from proto import Proto
+from flags import Flags
 
-class Challenge(object):
+class Challenge(Packet):
+    protocolVersion = 0x0a;
+    serverVersion = ""
+    connectionId = 0
+    challenge1 = ""
+    capabilityFlags = Flags.CLIENT_PROTOCOL_41
+    characterSet = 0
+    statusFlags = 0
+    challenge2 = ""
     
+    def setCapabilityFlag(self, flag):
+        self.capabilityFlags |= flag
+    
+    def removeCapabilityFlag(self, flag):
+        self.capabilityFlags &= ~flag
+    
+    def toggleCapabilityFlag(self, flag):
+        self.capabilityFlags ^= flag
+    
+    def hasCapabilityFlag(self, flag):
+        return ((self.capabilityFlags & flag) == flag)
+    
+    def setStatusFlag(self, flag):
+        self.statusFlags |= flag
+    
+    def removeStatusFlag(self, flag):
+        self.statusFlags &= ~flag
+    
+    def toggleStatusFlag(self, flag):
+        self.statusFlags ^= flag
+    
+    def hasStatusFlag(self, flag):
+        return ((self.statusFlags & flag) == flag)
+    
+    def getPayload(self):
+        payload = bytearray()
+        
+        payload.extend(Proto.build_fixed_int(1, self.protocolVersion))
+        payload.extend(Proto.build_null_str(self.serverVersion))
+        payload.extend(Proto.build_fixed_int(4, self.connectionId))
+        payload.extend(Proto.build_fixed_str(8, self.challenge1))
+        payload.extend(Proto.build_filler(1))
+        payload.extend(Proto.build_fixed_int(2, self.capabilityFlags))
+        payload.extend(Proto.build_fixed_int(1, self.characterSet))
+        payload.extend(Proto.build_fixed_int(2, self.statusFlags))
+        payload.extend(Proto.build_fixed_str(13, ""))
+        
+        if self.hasCapabilityFlag(Flags.CLIENT_SECURE_CONNECTION):
+            payload.extend( Proto.build_fixed_str(12, self.challenge2))
+            payload.extend( Proto.build_filler(1))
+        
+        return payload
+    
+    @staticmethod
+    def loadFromPacket(packet):
+        obj = Challenge();
+        proto = Proto(packet, 3);
+        
+        obj.sequenceId = proto.get_fixed_int(1)
+        obj.protocolVersion = proto.get_fixed_int(1)
+        obj.serverVersion = proto.get_null_str()
+        obj.connectionId = proto.get_fixed_int(4)
+        obj.challenge1 = proto.get_fixed_str(8)
+        proto.get_filler(1)
+        obj.capabilityFlags = proto.get_fixed_int(2)
+        obj.characterSet = proto.get_fixed_int(1)
+        obj.statusFlags = proto.get_fixed_int(2)
+        proto.get_filler(13)
+        
+        if (obj.hasCapabilityFlag(Flags.CLIENT_SECURE_CONNECTION)):
+            obj.challenge2 = proto.get_fixed_str(12)
+            proto.get_filler(1)
+        
+        return obj;
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
---
+++ src/mysql_proto/auth/response.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from packet import Packet
+from proto import Proto
+from flags import Flags
+
+class Response(Packet):
+    capabilityFlags = Flags.CLIENT_PROTOCOL_41
+    maxPacketSize = 0
+    characterSet = 0
+    username = ""
+    authResponse = ""
+    schema = ""
+    
+    def setCapabilityFlag(self, flag):
+        self.capabilityFlags |= flag
+    
+    def removeCapabilityFlag(self, flag):
+        self.capabilityFlags &= ~flag
+    
+    def toggleCapabilityFlag(self, flag):
+        self.capabilityFlags ^= flag
+    
+    def hasCapabilityFlag(self, flag):
+        return ((self.capabilityFlags & flag) == flag)
+    
+    def getPayload(self):
+        payload = bytearray()
+        
+        if self.hasCapabilityFlag(Flags.CLIENT_PROTOCOL_41):
+            payload.extend( Proto.build_fixed_int(4, self.capabilityFlags));
+            payload.extend( Proto.build_fixed_int(4, self.maxPacketSize));
+            payload.extend( Proto.build_fixed_int(1, self.characterSet));
+            payload.extend( Proto.build_fixed_str(23, ""));
+            payload.extend( Proto.build_null_str(self.username));
+            if this.hasCapabilityFlag(Flags.CLIENT_SECURE_CONNECTION):
+                payload.extend( Proto.build_lenenc_str(self.authResponse));
+            else:
+                payload.extend( Proto.build_null_str(self.authResponse));
+            payload.extend( Proto.build_fixed_str(len(self.schema), self.schema));
+        else:
+            payload.extend( Proto.build_fixed_int(2, self.capabilityFlags));
+            payload.extend( Proto.build_fixed_int(3, self.maxPacketSize));
+            payload.extend( Proto.build_null_str(self.username));
+            payload.extend( Proto.build_null_str(self.authResponse));
+        
+        return payload
+    
+    @staticmethod
+    def loadFromPacket(packet):
+        obj = Response()
+        proto = Proto(packet, 3)
+        
+        obj.sequenceId = proto.get_fixed_int(1)
+        obj.capabilityFlags = proto.get_fixed_int(2)
+        proto.offset -= 2
+        
+        if obj.hasCapabilityFlag(Flags.CLIENT_PROTOCOL_41):
+            obj.capabilityFlags = proto.get_fixed_int(4)
+            obj.maxPacketSize = proto.get_fixed_int(4)
+            obj.characterSet = proto.get_fixed_int(1)
+            proto.get_fixed_str(23)
+            obj.username = proto.get_null_str()
+            
+            if obj.hasCapabilityFlag(Flags.CLIENT_SECURE_CONNECTION):
+                obj.authResponse = proto.get_null_str()
+            else:
+                obj.authResponse = proto.get_lenenc_str()
+            
+            obj.schema = proto.get_eop_str()
+            
+        else:
+            obj.capabilityFlags = proto.get_fixed_int(2)
+            obj.maxPacketSize = proto.get_fixed_int(3)
+            obj.username = proto.get_null_str()
+            obj.schema = proto.get_null_str()
+        
+        return obj;
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
