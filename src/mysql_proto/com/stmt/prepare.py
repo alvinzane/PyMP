784315585e7840501b66d43311a64f9a5549a956
Rob Smith <kormoc@gmail.com>
2013-05-07 17:50:29 -0700

    Com_Stmt_Prepare

---
+++ src/mysql_proto/com/stmt/prepare.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from packet import Packet
+from proto import Proto
+from flags import Flags
+
+class Prepare(Packet):
+    query = ""
+    
+    def getPayload(self):
+        payload = bytearray()
+        
+        payload.extend(Proto.build_byte(Flags.COM_STMT_PREPARE))
+        payload.extend(Proto.build_eop_str(self.query))
+        
+        return payload
+    
+    @staticmethod
+    def loadFromPacket(packet):
+        obj = Statistics()
+        proto = Proto(packet, 3)
+        
+        obj.sequenceId = proto.get_fixed_int(1)
+        proto.get_filler(1)
+        obj.query = proto.get_eop_str()
+        
+        return obj
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
