5b19f71ca169be707cb956862567a2a48772d6d8
Rob Smith <kormoc@gmail.com>
2013-05-07 17:32:32 -0700

    Com_Shutdown

---
+++ src/mysql_proto/com/shutdown.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from packet import Packet
+from proto import Proto
+from flags import Flags
+
+class Shutdown(Packet):
+    shutdownType = Flags.SHUTDOWN_DEFAULT
+    
+    def getPayload(self):
+        payload = bytearray()
+        
+        payload.extend(Proto.build_byte(Flags.COM_SHUTDOWN))
+        if self.shutdownType != Flags.SHUTDOWN_DEFAULT:
+            payload.extend(Proto.build_fixed_int(1, self.shutdownType))
+        
+        return payload
+    
+    @staticmethod
+    def loadFromPacket(packet):
+        obj = Shutdown()
+        proto = Proto(packet, 3)
+        
+        obj.sequenceId = proto.get_fixed_int(1)
+        proto.get_filler(1)
+        obj.shutdownType = proto.get_fixed_int(1)
+        
+        return obj
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
