a58326409b93f8c1b11c1fda759b2851aa1933f9
Rob Smith <kormoc@gmail.com>
2013-05-07 17:52:30 -0700

    Com_Stmt_Reset

---
+++ src/mysql_proto/com/stmt/reset.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from packet import Packet
+from proto import Proto
+from flags import Flags
+
+class Reset(Packet):
+    data = bytearray()
+    
+    def getPayload(self):
+        payload = bytearray()
+        
+        payload.extend(self.data)
+        
+        return payload
+    
+    @staticmethod
+    def loadFromPacket(packet):
+        obj = Reset()
+        proto = Proto(packet, 3)
+        
+        obj.sequenceId = proto.get_fixed_int(1)
+        obj.data = proto.packet[proto.offset:]
+        
+        return obj
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
