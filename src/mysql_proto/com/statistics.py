dff72154510eced771ee70d215dec9cd56c80014
Rob Smith <kormoc@gmail.com>
2013-05-07 17:34:30 -0700

    Com_Statistics

---
+++ src/mysql_proto/com/statistics.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from packet import Packet
+from proto import Proto
+from flags import Flags
+
+class Statistics(Packet):
+    def getPayload(self):
+        payload = bytearray()
+        
+        payload.extend(Proto.build_byte(Flags.COM_STATISTICS))
+        
+        return payload
+    
+    @staticmethod
+    def loadFromPacket(packet):
+        obj = Statistics()
+        proto = Proto(packet, 3)
+        
+        obj.sequenceId = proto.get_fixed_int(1)
+        proto.get_filler(1)
+        
+        return obj
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
