5fb4e16af4b9b03f5ece07a1eb06c26e7ee297fe
Rob Smith <kormoc@gmail.com>
2013-05-07 17:51:31 -0700

    Com_Stmt_Prepare_Ok

---
+++ src/mysql_proto/com/stmt/prepare_ok.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from packet import Packet
+from proto import Proto
+from flags import Flags
+
+class Prepare_Ok(Packet):
+    
+    def getPayload(self):
+        payload = bytearray()
+        
+        return payload
+    
+    @staticmethod
+    def loadFromPacket(packet):
+        obj = Prepare_Ok()
+        proto = Proto(packet, 3)
+        
+        obj.sequenceId = proto.get_fixed_int(1)
+        
+        return obj
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
