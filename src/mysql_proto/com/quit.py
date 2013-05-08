8deca896a2600f59ef32c72addd97c6db355ac6a
Rob Smith <kormoc@gmail.com>
2013-05-07 17:06:14 -0700

    Com_Quit

---
+++ src/mysql_proto/com/quit.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from packet import Packet
+from proto import Proto
+from flags import Flags
+
+class Quit(Packet):
+
+    def getPayload(self):
+        payload = bytearray()
+        
+        payload.extend(Proto.build_byte(Flags.COM_QUIT))
+        
+        return payload
+    
+    @staticmethod
+    def loadFromPacket(packet):
+        obj = Quit()
+        proto = Proto(packet, 3)
+        
+        obj.sequenceId = proto.get_fixed_int(1)
+        proto.get_filler(1)
+        
+        return obj
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
