a4af260369a0c89402d8ec1c3ad0609c805f3a35
Rob Smith <kormoc@gmail.com>
2013-05-08 20:21:26 -0700

    Proxy basic code

--- mysql_proto/engine.py
+++ mysql_proto/engine.py
@@ -38,18 +38,24 @@ class Engine(multiprocessing.Process):
     mode = Flags.MODE_INIT
     nextMode = Flags.MODE_INIT
     
+    plugins = {}
+    
     def __init__(self, config, clientSocket):
         clientSocket = rebuild_handle(clientSocket)
         clientSocket = socket.fromfd(clientSocket,
                                      socket.AF_INET,
                                      socket.SOCK_STREAM)
         
+        clientSocket.setsockopt(socket.IPPROTO_TCP,
+                                socket.TCP_NODELAY,
+                                1)
+        
         self.config = config
         self.clientSocket = clientSocket
         self.kill_received = False
         
         self.logger = logging.getLogger('pymp')
-        formatter = logging.Formatter('[%(asctime)s] %(message)s')
+        formatter = logging.Formatter('%(process)d [%(asctime)s] %(message)s')
         streamHandler = logging.StreamHandler(sys.stdout)
         streamHandler.setFormatter(formatter)
         streamHandler.setLevel(logging.DEBUG)
@@ -64,47 +70,87 @@ class Engine(multiprocessing.Process):
         self.logger.debug('Accepted connection')
         
         multiprocessing.Process.__init__(self)
+        
+        if 'Proxy' in config['plugins']['enabled']:
+            from plugins.proxy import Proxy
+            self.plugins['Proxy'] = Proxy()
     
     def run(self):
         self.logger.debug('Running')
         while not self.kill_received:
             if self.mode == Flags.MODE_INIT:
                 self.logger.debug("MODE_INIT");
+                for plugin in self.plugins:
+                    self.plugins[plugin].init(self)
                 self.nextMode = Flags.MODE_READ_HANDSHAKE;
+                
             elif self.mode == Flags.MODE_READ_HANDSHAKE:
                 self.logger.debug("MODE_READ_HANDSHAKE");
+                for plugin in self.plugins:
+                    self.plugins[plugin].read_handshake(self)
                 self.nextMode = Flags.MODE_SEND_HANDSHAKE;
+                
             elif self.mode == Flags.MODE_SEND_HANDSHAKE:
                 self.logger.debug("MODE_SEND_HANDSHAKE");
+                for plugin in self.plugins:
+                    self.plugins[plugin].send_handshake(self)
                 self.nextMode = Flags.MODE_READ_AUTH;
+                
             elif self.mode == Flags.MODE_READ_AUTH:
                 self.logger.debug("MODE_READ_AUTH");
+                for plugin in self.plugins:
+                    self.plugins[plugin].read_auth(self)
                 self.nextMode = Flags.MODE_SEND_AUTH;
+                
             elif self.mode == Flags.MODE_SEND_AUTH:
                 self.logger.debug("MODE_SEND_AUTH");
+                for plugin in self.plugins:
+                    self.plugins[plugin].send_auth(self)
                 self.nextMode = Flags.MODE_READ_AUTH_RESULT;
+                
             elif self.mode == Flags.MODE_READ_AUTH_RESULT:
                 self.logger.debug("MODE_READ_AUTH_RESULT");
+                for plugin in self.plugins:
+                    self.plugins[plugin].read_auth_result(self)
                 self.nextMode = Flags.MODE_SEND_AUTH_RESULT;
+                
             elif self.mode == Flags.MODE_SEND_AUTH_RESULT:
                 self.logger.debug("MODE_SEND_AUTH_RESULT");
+                for plugin in self.plugins:
+                    self.plugins[plugin].send_auth_result(self)
                 self.nextMode = Flags.MODE_READ_QUERY;
+                
             elif self.mode == Flags.MODE_READ_QUERY:
                 self.logger.debug("MODE_READ_QUERY");
+                for plugin in self.plugins:
+                    self.plugins[plugin].read_query(self)
                 self.nextMode = Flags.MODE_SEND_QUERY;
+                
             elif self.mode == Flags.MODE_SEND_QUERY:
                 self.logger.debug("MODE_SEND_QUERY");
+                for plugin in self.plugins:
+                    self.plugins[plugin].send_query(self)
                 self.nextMode = Flags.MODE_READ_QUERY_RESULT;
+                
             elif self.mode == Flags.MODE_READ_QUERY_RESULT:
                 self.logger.debug("MODE_READ_QUERY_RESULT");
+                for plugin in self.plugins:
+                    self.plugins[plugin].read_query_result(self)
                 self.nextMode = Flags.MODE_SEND_QUERY_RESULT;
+                
             elif self.mode == Flags.MODE_SEND_QUERY_RESULT:
                 self.logger.debug("MODE_SEND_QUERY_RESULT");
+                for plugin in self.plugins:
+                    self.plugins[plugin].send_query_result(self)
                 self.nextMode = Flags.MODE_READ_QUERY;
+                
             elif self.mode == Flags.MODE_CLEANUP:
                 self.logger.debug("MODE_CLEANUP");
+                for plugin in self.plugins:
+                    self.plugins[plugin].cleanup(self)
                 self.nextMode = Flags.MODE_CLEANUP;
                 self.kill_received = True
+                
             else:
                 self.logger.fatal("UNKNOWN MODE "+self.mode);
                 self.kill_received = True
---
+++ mysql_proto/plugins/proxy.py
@@ -0,0 +1,115 @@
+# coding=utf-8
+
+from plugin import Plugin
+import socket
+from ..packet import Packet
+from ..auth.challenge import Challenge
+from ..auth.response import Response
+from ..flags import Flags
+from ..resultset import ResultSet
+
+class Proxy(Plugin):
+    serverSocket = None
+    
+    def init(self, context):
+        context.logger.debug('Proxy.init')
+        self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.serverSocket.setsockopt(socket.IPPROTO_TCP,
+                                     socket.TCP_NODELAY,
+                                     1)
+        self.serverSocket.connect((context.config[
+            'plugins']['Proxy']['remoteHost'],
+                                   int(
+                                    context.config[
+                                        'plugins']['Proxy']['remotePort']
+                                    )))
+    
+    def read_handshake(self, context):
+        context.logger.debug('Proxy.read_handshake')
+        packet = Packet.read_packet(self.serverSocket)
+        context.authChallenge = Challenge.loadFromPacket(packet)
+        context.authChallenge.removeCapabilityFlag(Flags.CLIENT_COMPRESS)
+        context.authChallenge.removeCapabilityFlag(Flags.CLIENT_SSL)
+        context.authChallenge.removeCapabilityFlag(Flags.CLIENT_LOCAL_FILES)
+        
+        ResultSet.characterSet = context.authChallenge.characterSet
+        
+        context.buff.extend(context.authChallenge.toPacket())
+    
+    def send_handshake(self, context):
+        context.logger.debug('Proxy.send_handshake')
+        context.clientSocket.sendall(context.buff)
+        context.buff = bytearray()
+    
+    def read_auth(self, context):
+        context.logger.debug('Proxy.read_auth')
+        packet = Packet.read_packet(context.clientSocket)
+        context.authReply = Response.loadFromPacket(packet)
+        
+        if not context.authReply.hasCapabilityFlag(Flags.CLIENT_PROTOCOL_41):
+            this.logger.fatal('We do not support Protocols under 4.1')
+            context.kill_received = True
+            return
+        
+        context.authReply.removeCapabilityFlag(Flags.CLIENT_COMPRESS)
+        context.authReply.removeCapabilityFlag(Flags.CLIENT_SSL)
+        context.authReply.removeCapabilityFlag(Flags.CLIENT_LOCAL_FILES)
+        
+        context.schema = context.authReply.schema
+        
+        context.buff.extend(context.authReply.toPacket())
+    
+    def send_auth(self, context):
+        context.logger.debug('Proxy.send_auth')
+        self.serverSocket.sendall(context.buff)
+        context.buff = bytearray()
+    
+    def read_auth_result(self, context):
+        context.logger.debug('Proxy.read_auth_result')
+        packet = Packet.read_packet(self.serverSocket)
+        if Packet.getType(packet) != Flags.OK:
+            this.logger.fatal('Auth is not okay!')
+        context.buff.extend(packet)
+    
+    def send_auth_result(self, context):
+        context.logger.debug('Proxy.send_auth_result')
+        context.clientSocket.sendall(context.buff)
+        context.buff = bytearray()
+    
+    def read_query(self, context):
+        context.logger.debug('Proxy.read_query')
+        context.bufferResultSet = false
+        
+        packet = Packet.read_packet(context.clientSocket)
+        context.sequenceId = Packet.getSequenceId(packet)
+        this.logger.debug('Client sequenceId: %s' % context.sequenceId)
+        
+        packet_type = Packet.getType(packet)
+        
+        if packet_type == Flags.COM_QUIT:
+            context.logger.trace('COM_QUIT')
+            context.kill_received = True
+        elif packet_type == Flags.COM_INIT_DB:
+            context.logger.trace('COM_INIT_DB');
+            context.schema = Com_Initdb.loadFromPacket(packet).schema
+        elif packet_type == Flags.COM_QUERY:
+            context.logger.debug('COM_QUERY')
+            context.query = Com_Query.loadFromPacket(packet).query
+    
+    def send_query(self, context):
+        context.logger.debug('Proxy.send_query')
+        self.serverSocket.sendall(context.buff)
+        context.buff = bytearray()
+    
+    def read_query_result(self, context):
+        context.logger.debug('Proxy.read_query_result')
+        packet = Packet.read_packet(self.serverSocket)
+    
+    def send_query_result(self, context):
+        context.logger.debug('Proxy.send_query_result')
+        context.clientSocket.sendall(context.buff)
+        context.buff = bytearray()
+    
+    def cleanup(self, context):
+        context.logger.debug('Proxy.cleanup')
+        context.kill_received = True
